374853737dc5ad843086b810544228d30004af6e
JulieChabanis <julie.chabanis07@gmail.com>
2023-02-21 12:44:02 +0100

    Add ThemeProvider D/L

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import AgencyComponent from './components/AgencyComponent';
-
+import { ColorModeContext, useMode } from './components/UI/Themes/theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
-return (
-  <AgencyComponent/>
-)
+  const [theme, colorMode] = useMode(); 
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className='app'> 
+          <main className='content'>
+          <AgencyComponent/>
+          </main>
+        </div>
+        </ThemeProvider>
+    </ColorModeContext.Provider>
+  ); 
 }
 
 export default App;
