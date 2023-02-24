463bacaccd5405ec8ccef17354fbb8854e01975a
JulieChabanis <julie.chabanis07@gmail.com>
2023-02-24 13:11:08 +0100

    add onChange e

--- src/components/Tenants/Requests/CreateTenant.jsx
+++ src/components/Tenants/Requests/CreateTenant.jsx
@@ -1,13 +1,35 @@
-import React, { useState, useEffect} from 'react'; 
+import React, { useState, useEffect,} from 'react'; 
 import { FormControl, Box, Typography, Button } from '@mui/material';
 import { InputLabel, Input, FormHelperText, TextField } from '@mui/material';
 import { tokens } from '../../UI/Themes/theme';
 import { useTheme } from '@emotion/react';
+import axios from 'axios';
 // import TenantService from '../../../api/TenantService';
 
 export default function CreateTenantForm() {
+
   const theme = useTheme();
   const colors = tokens(theme.palette.mode); 
+
+  const [tenant, setTenant] = useState({
+    name:'',
+    lastname:'',
+    email:'',
+    phone:'',
+  })
+
+  const {name, lastname, email, phone} = tenant;
+
+
+  const onInputChange = (e) => {
+
+    setTenant({...tenant, [e.target.name]: e.target.value})
+  }; 
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:8080/api/tenants/create', tenant);
+  }
 
   
   return (
@@ -31,22 +53,24 @@ export default function CreateTenantForm() {
       </Typography>
       <Box
         component='form'
+        onSubmit={(e) => onSubmit(e)}
       >
         <FormControl fullWidth>
             <TextField 
             margin='normal'
+            required
             id="name"
             label="Nom" 
             variant="outlined"
-            autoFocus
+            onChange={(e)=> onInputChange(e)}
             >
             </TextField>
             <br /> 
-            <TextField id="outlined-basid" label="Prénom" variant="outlined"></TextField>
+            <TextField required id="outlined-basid" label="Prénom" variant="outlined" onChange={(e)=> onInputChange(e)}></TextField>
             <br /> 
-            <TextField id="outlined-basid" label="Email" variant="outlined"></TextField>
+            <TextField required id="outlined-basid" label="Email" variant="outlined" onChange={(e)=> onInputChange(e)}></TextField>
             <br /> 
-            <TextField id="outlined-basid" label="Telephone" variant="outlined"></TextField>
+            <TextField required id="outlined-basid" label="Telephone" variant="outlined" onChange={(e)=> onInputChange(e)}></TextField>
         </FormControl>
         <Button
           type="submit"
